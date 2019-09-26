a24c968f28c553bbdc6bd08d7fb0ba1aaaea4648
YJL <steve_stone@126.com>
2019-09-26 19:22:53 +0800

    add regular expression match

---
+++ algorithm/sword-19-regular_expression_matching(正则匹配).cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+// 面试题19：正则表达式匹配
+// 题目：请实现一个函数用来匹配包含'.'和'*'的正则表达式。模式中的字符'.'
+// 表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）。在本题
+// 中，匹配是指字符串的所有字符匹配整个模式。例如，字符串"aaa"与模式"a.a"
+// 和"ab*ac*a"匹配，但与"aa.a"及"ab*a"均不匹配。
+// 《剑指offer》
+
+// 正则表达式匹配模式核心函数，用于判断字符串 str 和模式 pattern 是否匹配
+bool matchCore(const char* str, const char* pattern){
+    if(*str == '\0' && *pattern == '\0'){
+        return true;
+    }
+    if(*str != '\0' && *pattern == '\0'){
+        return false;
+    }
+    if(*(pattern + 1) == '*'){   // 下一字符是 '*' 需分 3 种情况考虑
+        if(*pattern == *str || (*pattern == '.' && *str != '\0')){  // str 的当前字符与模式匹配成功
+            return matchCore(str + 1, pattern + 2)  // 跳过 '*'
+                || matchCore(str + 1, pattern)      // 不跳过 '*' 继续使用 '*' 进行匹配
+                || matchCore(str, pattern + 2);     // 不使用 '*' 进行匹配，因为 '*' 前的字符可以出现 0 次
+        }else{                                      // str 的当前字符与模式匹配失败
+            return matchCore(str, pattern + 2);     // 不使用 '*' 进行匹配，因为 '*' 前的字符可以出现 0 次
+        }
+    }
+    if(*str == *pattern || (*pattern == '.' && *str != '\0')){  // 其他匹配成功的情况
+        return matchCore(str + 1, pattern + 1);
+    }
+    return false;
+}
+
+// 调用匹配函数，判断字符串 str 和模式 pattern 是否匹配
+bool isMatch(const char* str, const char* pattern){
+    if(str == nullptr || pattern == nullptr){
+        return false;
+    }
+    return matchCore(str, pattern);
+}
+
+// ====================测试代码====================
+void Test(const char* testName, const char* str, const char* pattern, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    if(isMatch(str, pattern) == expected)
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+int main(){
+    Test("Test01", "", "", true);
+    Test("Test02", "", ".*", true);
+    Test("Test03", "", ".", false);
+    Test("Test04", "", "c*", true);
+    Test("Test05", "a", ".*", true);
+    Test("Test06", "a", "a.", false);
+    Test("Test07", "a", "", false);
+    Test("Test08", "a", ".", true);
+    Test("Test09", "a", "ab*", true);
+    Test("Test10", "a", "ab*a", false);
+    Test("Test11", "aa", "aa", true);
+    Test("Test12", "aa", "a*", true);
+    Test("Test13", "aa", ".*", true);
+    Test("Test14", "aa", ".", false);
+    Test("Test15", "ab", ".*", true);
+    Test("Test16", "ab", ".*", true);
+    Test("Test17", "aaa", "aa*", true);
+    Test("Test18", "aaa", "aa.a", false);
+    Test("Test19", "aaa", "a.a", true);
+    Test("Test20", "aaa", ".a", false);
+    Test("Test21", "aaa", "a*a", true);
+    Test("Test22", "aaa", "ab*a", false);
+    Test("Test23", "aaa", "ab*ac*a", true);
+    Test("Test24", "aaa", "ab*a*c*a", true);
+    Test("Test25", "aaa", ".*", true);
+    Test("Test26", "aab", "c*a*b", true);
+    Test("Test27", "aaca", "ab*a*c*a", true);
+    Test("Test28", "aaba", "ab*a*c*a", false);
+    Test("Test29", "bbbba", ".*a*a", true);
+    Test("Test30", "bcbbabab", ".*a*a", false);
+
+    return 0;
+}
